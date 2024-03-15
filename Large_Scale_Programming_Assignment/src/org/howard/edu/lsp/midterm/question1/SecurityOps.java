7209bb13c5d63d35044769faa2da71a979793bf8
divyaa370 <132010147+divyaa370@users.noreply.github.com>
2024-03-14 20:47:10 -0400

    Add files via upload

--- Large_Scale_Programming_Assignment/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
+++ Large_Scale_Programming_Assignment/src/org/howard/edu/lsp/midterm/question1/SecurityOps.java
@@ -5,20 +5,23 @@ public class SecurityOps {
         // Remove whitespace from the input text
         text = text.replaceAll("\\s", "");
 
-        StringBuilder evenChars = new StringBuilder();
-        StringBuilder oddChars = new StringBuilder();
+        StringBuilder evenChars = new StringBuilder();  // StringBuilder to store even-indexed characters
+        StringBuilder oddChars = new StringBuilder();  // StringBuilder to store odd-indexed characters
 
+     // Iterate over each character in the input text
         for (int i = 0; i < text.length(); i++) {
-            char ch = text.charAt(i);
-            if (Character.isLetterOrDigit(ch)) {
+            char ch = text.charAt(i); // Get the current character
+         
+            if (Character.isLetterOrDigit(ch)) { // Checks if the character is a letter or digit
+            	// Append the character to the appropriate StringBuilder based on its index
                 if (i % 2 == 0) {
-                    evenChars.append(ch);
+                    evenChars.append(ch); // Append to even-indexed characters StringBuilder
                 } else {
-                    oddChars.append(ch);
+                    oddChars.append(ch);  // Append to odd-indexed characters StringBuilder
                 }
             }
         }
-
+        // Concatenate the even and odd indexed characters to form the encrypted text
         return evenChars.toString() + oddChars.toString();
     }
 }
