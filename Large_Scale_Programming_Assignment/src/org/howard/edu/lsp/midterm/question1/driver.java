c25a9fc0cc3e4777848b72b312b36b9e3456d8fe
divyaa370 <132010147+divyaa370@users.noreply.github.com>
2024-05-02 21:10:47 -0400

    Delete Large_Scale_Programming_Assignment/src/org/howard/edu/lsp directory

--- Large_Scale_Programming_Assignment/src/org/howard/edu/lsp/midterm/question1/driver.java
+++
@@ -1,9 +0,0 @@
-package org.howard.edu.lsp.midterm.question1;
-
-public class driver {
-    public static void main(String[] args) {
-        String text = "I love CSCI363";
-        String encryptedText = SecurityOps.encrypt(text);
-        System.out.println("Encrypted text: \"" + encryptedText + "\"");
-    }
-}
